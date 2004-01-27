7ce612408688a9c9b42300ac0efbd48bc704999a
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-01-27 17:36:16 +0000

    import

---
+++ trunk/pl-j/src/blocks/chanells/febe/src/java/org/plj/chanells/febe/msg/ErrorMessageFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Created on Jan 18, 2004
+ */
+package org.plj.chanells.febe.msg;
+
+import java.io.IOException;
+
+import org.pgj.messages.Message;
+import org.plj.chanells.febe.core.Encoding;
+import org.plj.chanells.febe.core.PGStream;
+
+/**
+ * Creates error message.
+ * @author Laszlo Hornyak
+ * @version 0.1
+ */
+public class ErrorMessageFactory implements MessageFactory {
+
+	public static final int MESSAGE_HEADER_ERROR = 'E';
+
+	/* (non-Javadoc)
+	 * @see org.plj.chanells.febe.msg.MessageFactory#getMessageHeader()
+	 */
+	public int getMessageHeader() {
+		return 'E';
+	}
+
+	/* (non-Javadoc)
+	 * @see org.plj.chanells.febe.msg.MessageFactory#getMessage(org.plj.chanells.febe.core.PGStream)
+	 */
+	public Message getMessage(PGStream stream, Encoding encoding) throws IOException{
+		org.pgj.messages.Exception msg = new org.pgj.messages.Exception();
+		msg.setMessage(stream.ReceiveString(encoding));
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.plj.chanells.febe.msg.MessageFactory#sendMessage()
+	 */
+	public void sendMessage(Message msg, PGStream stream) throws IOException {
+		stream.SendChar(MESSAGE_HEADER_ERROR);
+		stream.Send(((org.pgj.messages.Exception)msg).getMessage().getBytes());
+
+	}
+
+}
