886bf53add1513e80f5cf49f04176e4710ad5ef0
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-02-11 20:26:58 +0000

    up to java 1.4

--- pl-j/src/interfaces/org/pgj/typemapping/MappingException.java
+++ pl-j/src/interfaces/org/pgj/typemapping/MappingException.java
@@ -7,6 +7,23 @@ public class MappingException extends Exception {
 	public MappingException() {
 		super();
 	}
+	/**
+	 * @param cause
+	 */
+	public MappingException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public MappingException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
 	public MappingException(String msg) {
 		super(msg);
 	}
