93403eb2a98ebd68acefc990b6f28ece3384577d
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-02-22 22:46:06 +0000

    from a lost file

---
+++ trunk/pl-j/src/blocks/chanells/febe/src/java/org/plj/chanells/febe/msg/CallMessageFactory.java
@@ -0,0 +1,74 @@
+/*
+ * Created on Jan 27, 2004
+ */
+package org.plj.chanells.febe.msg;
+
+import java.io.IOException;
+
+import org.pgj.messages.CallRequest;
+import org.pgj.messages.Message;
+import org.pgj.typemapping.Field;
+import org.pgj.typemapping.MappingException;
+import org.pgj.typemapping.TypeMapper;
+import org.plj.chanells.febe.core.Encoding;
+import org.plj.chanells.febe.core.PGStream;
+
+/**
+ * Creates call messages.
+ * @author Laszlo Hornyak
+ * @version 0.1
+ */
+public class CallMessageFactory implements MessageFactory {
+
+	TypeMapper typeMapper = null;
+
+	public CallMessageFactory(TypeMapper typeMapper) {
+		this.typeMapper = typeMapper;
+	}
+
+	public static final int MESSAGE_HEADER_CALL = 'C';
+
+	/* (non-Javadoc)
+	 * @see org.plj.chanells.febe.msg.MessageFactory#getMessageHeader()
+	 */
+	public int getMessageHeader() {
+		return MESSAGE_HEADER_CALL;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.plj.chanells.febe.msg.MessageFactory#getMessage(org.plj.chanells.febe.core.PGStream, org.plj.chanells.febe.core.Encoding)
+	 */
+	public Message getMessage(PGStream stream, Encoding encoding)
+		throws IOException, MappingException {
+
+		CallRequest request = new CallRequest();
+		request.setClassname(stream.ReceiveString(encoding));
+		request.setMethodname(stream.ReceiveString(encoding));
+		int paramcount = stream.ReceiveInteger(4);
+
+		for (int i = 0; i < paramcount; i++) {
+			String paramType = stream.ReceiveString(encoding);
+			int paramSize = stream.ReceiveInteger(4);
+			byte[] paramData = stream.Receive(paramSize);
+			Field fld = typeMapper.map(paramData, paramType);
+			request.addParam(fld);
+		}
+
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.plj.chanells.febe.msg.MessageFactory#sendMessage(org.pgj.messages.Message, org.plj.chanells.febe.core.PGStream)
+	 */
+	public void sendMessage(Message msg, PGStream stream) throws IOException {
+		//TODO: this should throw something error!
+	}
+
+	/* (non-Javadoc)
+	 * @see org.plj.chanells.febe.msg.MessageFactory#getHandledClassname()
+	 */
+	public String getHandledClassname() {
+		return CallRequest.class.getName();
+	}
+
+}
