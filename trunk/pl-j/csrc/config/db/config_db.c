56a084ee93b28197e1c44175ce51d44622627fc0
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-02-22 22:40:01 +0000

    not yet tested

--- trunk/pl-j/csrc/config/db/config_db.c
+++ trunk/pl-j/csrc/config/db/config_db.c
@@ -1,9 +1,30 @@
 
 #include "module_config.h"
+#include "executor/spi.h"
 
-char* plj_get_configvalue_string(const char* paramName){
+const char* get_sql = 
+	"SELECT config_value FROM SQLJ.plj_config where config_key='%s'";
+
+char* plj_get_configvalue_string(const char* paramName) {
+
+	char* sql;
+	int proc, ret;
+
+	//no SPI_connect, we are already connected.
+
+	sql = SPI_palloc(strlen(paramName) + strlen(get_sql) );
 	
-	return "dummy :)";
+	sprintf(sql,get_sql, paramName);
+
+	ret = SPI_exec(sql,1);
+	proc = SPI_processed;
+	if( ret == SPI_OK_SELECT && proc > 0){
+		TupleDesc tupdesc = SPI_tuptable->tupdesc;
+		SPITupleTable *tuptable = SPI_tuptable;
+		return SPI_getvalue(tuptable->vals[0], tupdesc, 1);
+	}
+
+	return "";
 }
 
 int plj_get_configvalue_int(const char* paramName){
@@ -11,6 +32,6 @@ int plj_get_configvalue_int(const char* paramName){
 }
 
 int plj_get_configvalue_boolean(const char* paramName){
-	return (strcmp("true",plj_get_configvalue_string(paramName) == 0));
+	return (strcmp("true",plj_get_configvalue_string(paramName)) == 0);
 }
 
