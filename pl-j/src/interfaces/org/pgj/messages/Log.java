2620a7e84987d0d20ed19da7e85221b354cc6926
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-02-28 21:47:32 +0000

    Log message type (relatively new)

---
+++ pl-j/src/interfaces/org/pgj/messages/Log.java
@@ -0,0 +1,62 @@
+/*
+ * Created on Feb 27, 2004
+ */
+
+package org.pgj.messages;
+
+/**
+ * Log message.
+ * @author Laszlo Hornyak
+ */
+public class Log extends Message {
+
+	public static final int LEVEL_DEBUG = 1;
+	public static final int LEVEL_WARN = 2;
+	public static final int LEVEL_ERROR = 3;
+	public static final int LEVEL_FATAL = 4;
+	private int level;
+	private String category = null;
+	private String message;
+
+	/**
+	 * @return Returns the category.
+	 */
+	public String getCategory() {
+		return category;
+	}
+
+	/**
+	 * @param category The category to set.
+	 */
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	/**
+	 * @return Returns the level.
+	 */
+	public int getLevel() {
+		return level;
+	}
+
+	/**
+	 * @param level The level to set.
+	 */
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	/**
+	 * @return Returns the message.
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message The message to set.
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
