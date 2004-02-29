d063d75b3279fb517b38159714c255b0c97200a6
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-02-29 08:34:26 +0000

    *** empty log message ***

--- pl-j/csrc/modules/febe/febe_channel_work.c
+++ pl-j/csrc/modules/febe/febe_channel_work.c
@@ -14,10 +14,119 @@
 #include <executor/spi.h>
 #include <regex/regex.h>
 
-int plpgj_chanell_send(message msg){
+#include "executor/spi.h"
+#include "utils/elog.h"
 
+#include "libpq-mini-misc.h"
+
+extern PGconn_min* min_conn;
+
+int febe_send_call(callreq call){
+	
+	elog(DEBUG1, "sending call req. (test phase)");
+	pqPutMsgStart(0,0,min_conn);
+	elog(DEBUG1, "pqPutMsgStart");
+	pqPutc('C',min_conn);
+	elog(DEBUG1, "putc");
+	pqPuts((const char*)call -> classname, min_conn);
+	elog(DEBUG1, "puts 1");
+	pqPuts(call -> methodname, min_conn);
+	elog(DEBUG1, "puts 2");
+	pqPuts(call -> expect, min_conn);
+	elog(DEBUG1, "puts 3");
+	//TODO: hmm?
+	pqPutInt(0,  4, min_conn);
+	elog(DEBUG1, "putint 1");
+	pqPutMsgEnd(min_conn);
+	elog(DEBUG1, "message end");
+	pqFlush(min_conn);
+	elog(DEBUG1, "flush, done");
+	return 0;
 }
 
-message plpgj_chanell_receive(void){
+int febe_send_result(plpgj_result res){
+	elog(DEBUG1, "sending result. (not impl)");
+	return 0;
+}
+
+int febe_send_exception(error_message err){
+	elog(DEBUG1, "sending exception. (not impl)");
+	return 0;
+}
+
+int plpgj_channel_send(message msg){
+	switch(msg->msgtype){
+		case MT_CALLREQ:
+			return febe_send_call((callreq)msg);
+		case MT_RESULT:
+			return febe_send_result((plpgj_result)msg);
+		case MT_EXCEPTION:
+			return febe_send_exception((error_message)msg);
+		default:
+		break;
+	}
+}
+
+void* febe_receive_exception(){
+	PQExpBuffer name, mesg;
+	int res;
+	error_message ret;
+
+	elog(DEBUG1, "febe_receive_exception");
+
+	name = createPQExpBuffer();
+	mesg = createPQExpBuffer();
+
+	pqReadData(min_conn);
+
+	elog(DEBUG1, "febe_receive_exception tp 1");
+	res = pqGets(name, min_conn);
+
+	res = pqGets(mesg, min_conn);
+
+	elog(DEBUG1, "febe_receive_exception tp 2");	
+
+	ret = malloc(sizeof(str_error_message));
+
+	ret -> classname = name -> data;
+	ret -> message = mesg -> data;
+	ret -> stacktrace = NULL;
+
+	elog(DEBUG1, "febe_receive_exception tp 3");
+
+	return ret;
+}
+
+void* febe_receive_result(){
+	return NULL;
+}
+
+message plpgj_channel_receive(void){
+	int header;
+	char type;
+	int ret;
+	sleep(10);
+	
+	ret = pqGetInt(&header, 4, min_conn);
+
+	elog(DEBUG1, "header: %d", header);
+
+	ret = pqGetc(&type, min_conn);
+
+	if(ret == EOF){
+		elog(ERROR, "pqGetc returned EOF");
+	}
+
+	switch(type){
+		case 'R':
+			return (message) febe_receive_result();
+		case 'E':
+			return (message) febe_receive_exception();
+		default:
+			elog(ERROR, "message type unknown :%d", type);
+			return NULL;
+	}
+
+	return NULL;
 }
 
