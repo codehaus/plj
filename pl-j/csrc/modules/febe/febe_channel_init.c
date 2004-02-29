e9322bf129459bc22352b5f02f430d2f20f58e7e
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-02-29 08:19:26 +0000

    bugfix, protocol trace file option

--- pl-j/csrc/modules/febe/febe_channel_init.c
+++ pl-j/csrc/modules/febe/febe_channel_init.c
@@ -9,6 +9,7 @@
 #include "module_config.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int	PLJ_FEBE_INITED		= 0;
 char*	PLJ_JAVA_HOST		= NULL;
@@ -18,10 +19,13 @@ int	PLJ_PORT		= -1;
 
 #define CONFIG_COPY(a,b) tmp=plj_get_configvalue_string(a);\
 	b = malloc(strlen(tmp));\
-	strcpy(a,tmp);
+	strcpy(b,tmp);
+
+struct PGconn_min* min_conn = NULL;
 
 int plpgj_channel_initialize(){
 	char* tmp;
+	char* tracefile;
 
 	CONFIG_COPY("febe-mini.host",PLJ_JAVA_HOST)
 
@@ -30,7 +34,20 @@ int plpgj_channel_initialize(){
 	CONFIG_COPY("febe-mini.unix-socket",PLJ_UNIX_SOCKET)
 	CONFIG_COPY("febe-mini.connect-timeout",PLJ_CONNECT_TIMEOUT)
 
-	PLJ_FEBE_INITED = 1;
+
+	elog(DEBUG1, "configured");
+	min_conn = (struct PGconn_min*)pq_min_connect();
+	elog(DEBUG1, "connected");
+
+	tracefile = plj_get_configvalue_string("febe-mini.proto-trace");
+	if(strcmp(tracefile,"") != 0){
+		elog(DEBUG2,"using tracefile: %s", tracefile);
+		pq_min_set_trace(min_conn, fopen(tracefile,"a"));
+	}
+
+	if(min_conn != NULL)
+		PLJ_FEBE_INITED = 1;
+
 }
 
 int plpgj_channel_initialized(){
