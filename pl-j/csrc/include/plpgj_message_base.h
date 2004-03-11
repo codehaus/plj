d906a6fecdba31920bef6a454c6a88b4c1054cb9
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-03-11 22:16:34 +0000

    some structures moved here

--- pl-j/csrc/include/plpgj_message_base.h
+++ pl-j/csrc/include/plpgj_message_base.h
@@ -5,8 +5,19 @@
 	unsigned short msgtype;\
 	unsigned int length;
 
-typedef struct str_message{
+typedef struct str_message {
 	base_message_content
 }str_message, *message;
+
+typedef struct str_raw {
+        int length;
+        unsigned short isnull;
+        void *data;
+}*raw;
+
+typedef struct fnc_param {
+	char* type;
+	struct str_raw data;
+}*pparam;
 
 #endif
