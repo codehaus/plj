df77fae81c97d46fc23cdbbf0103970dae1697ac
lazlo <lazlo@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-03-13 00:27:20 +0000

    doco reorg

--- pl-j/src/jdbc/org/pgj/jdbc/scratch/PGJJDBCConnection.java
+++ pl-j/src/jdbc/org/pgj/jdbc/scratch/PGJJDBCConnection.java
@@ -1,8 +1,5 @@
 /*
  * Created on Jun 12, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
 package org.pgj.jdbc.scratch;
 
