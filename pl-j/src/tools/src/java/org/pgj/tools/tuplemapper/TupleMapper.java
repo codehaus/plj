4d5a80e62fbe6fcecedf8d29865734b97593df16
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-06-18 19:57:16 +0000

    doco

--- pl-j/src/tools/src/java/org/pgj/tools/tuplemapper/TupleMapper.java
+++ pl-j/src/tools/src/java/org/pgj/tools/tuplemapper/TupleMapper.java
@@ -9,15 +9,34 @@ import org.pgj.typemapping.Tuple;
 
 
 /**
- * Maps tuples to objects.
+ * Maps tuples to objects and back.
+ * Implementations will tipically need a typemapper to accomplish their job.
+ * 
  * @author Laszlo Hornyak
  */
 public interface TupleMapper {
 
-	/** Map a tuple to a java object. */
+	/**
+	 * Map a tuple to a java object.
+	 * @param tuple the tuple to be mapped to bean
+	 * @return the bean representing the tuple
+	 * @throws MappingException	if the tuple can`t be mapped to a bean
+	 */
 	Object mapTuple(Tuple tuple) throws MappingException;
 
-	/** Get the mapped class for a given tuple. */
-	Class getMappedClass(Tuple tuple);
+	/**
+	 * Get the mapped class for a given tuple.
+	 * @param tuple	the tuple to be mapped to bean
+	 * @return the class of the bean
+	 * @throws MappingException if the tuple can not be mapped to a class
+	 */
+	Class getMappedClass(Tuple tuple) throws MappingException;
 
+	/**
+	 * Maps an object back to a tupple.
+	 * @param obj the object to be mapped as tuple
+	 * @return the tuple
+	 * @throws MappingException if the object couldn`t be mapped back to tuple.
+	 */
+	Tuple backMap(Object obj) throws MappingException;
 }
