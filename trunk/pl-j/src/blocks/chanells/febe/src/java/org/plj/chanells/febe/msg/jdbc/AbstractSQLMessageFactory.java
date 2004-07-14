399936e7a4b02fb61b2880a8c035e8250686528f
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-07-14 00:52:15 +0000

    *** empty log message ***

---
+++ trunk/pl-j/src/blocks/chanells/febe/src/java/org/plj/chanells/febe/msg/jdbc/AbstractSQLMessageFactory.java
@@ -0,0 +1,25 @@
+/*
+ * Created on Jul 14, 2004
+ */
+
+package org.plj.chanells.febe.msg.jdbc;
+
+import org.plj.chanells.febe.msg.MessageFactory;
+
+
+/**
+ * Base of the message factoryes sending SQL messages.
+ * @author Laszlo Hornyak
+ */
+abstract class AbstractSQLMessageFactory implements MessageFactory {
+
+	/*
+	 * See plpgj_message_sql_h to see it's pair. 
+	 */
+	public static final int SQLTYPE_STATEMENT = 1;
+	public static final int SQLTYPE_CURSORCLOSE = 2;
+	public static final int SQLTYPE_FETCH = 3;
+	public static final int SQLTYPE_CURSOR_OPEN = 4;
+
+	public abstract int getSQLType();
+}
