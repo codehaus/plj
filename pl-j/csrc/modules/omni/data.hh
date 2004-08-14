b43e17ae9b0a58e20adb6f3076ca2cf1191f0fb9
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-08-14 09:35:21 +0000

    good bye bad old communication modules

--- pl-j/csrc/modules/omni/data.hh
+++
@@ -1,8 +0,0 @@
-
-#include "omniORB4/CORBA.h"
-#include "callserver.hh"
-
-extern org::pgj::corba::CallServer::_ptr_type callserver;
-extern CORBA::ORB_ptr omni_orb;
-extern int initialized;
-
--- pl-j/csrc/modules/orbit/skeletons/callreq.h
+++
@@ -1,3 +0,0 @@
-
-#include <workaround.h>
-
