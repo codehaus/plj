1f8b3cae0e9fdfe3a68a1ce15e3c750bfc39421f
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-08-29 17:58:32 +0000

    new test udfs

--- pl-j/etc/install/install_test.sql
+++ pl-j/etc/install/install_test.sql
@@ -82,6 +82,48 @@ as '
 ' language 'plj';
 
 
+-- Boolean type tests
+
+create or replace function plpgj_test_boolean1(bool)
+returns void
+as '
+        class=org.deadcat.typetests.BooleanTests
+        method=setBoolean
+' language 'plj';
+
+create or replace function plpgj_test_boolean2()
+returns bool
+as '
+        class=org.deadcat.typetests.BooleanTests
+        method=getBoolean
+' language 'plj';
+
+create or replace function plpgj_test_boolean_AND(bool, bool)
+returns bool
+as '
+        class=org.deadcat.typetests.BooleanTests
+        method=doAND
+' language 'plj';
+
+create or replace function plpgj_test_boolean_OR(bool, bool)
+returns bool
+as '
+        class=org.deadcat.typetests.BooleanTests
+        method=doOR
+' language 'plj';
+
+create or replace function plpgj_test_boolean_NOT(bool)
+returns bool
+as '
+        class=org.deadcat.typetests.BooleanTests
+        method=doNOT
+' language 'plj';
+
+
+
+
+-- JDBC tests
+
 create table plj_testtable(
 	id	int primary key,
 	name	varchar
@@ -99,3 +141,11 @@ create trigger plpgj_test_lametrigger1 before insert on plj_testtable
 	for each row
 	execute procedure plpgj_test_lametrigger1fn();
 
+create or replace function plpgj_test_jdbctest1()
+returns varchar
+as '
+        class=org.deadcat.jdbctests.JDBCTests
+        method=doTest1
+        oneway=false
+        instantiation=session
+' language 'plj';
