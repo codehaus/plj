8c2091a3a5d0073478230405113a82fe7a212fbb
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-08-31 20:28:59 +0000

    *** empty log message ***

--- pl-j/csrc/include/plpgj_message_transaction.h
+++ pl-j/csrc/include/plpgj_message_transaction.h
@@ -1,11 +1,9 @@
 #ifndef PLPGJ_MESSAGE_TRANSEVENT_H
 #define PLPGJ_MESSAGE_TRANSEVENT_H
 
-/*	*/
-
-/*	Transaction events */
-
-/*	*/
+/*
+ *	Transaction events 
+ */
 #define PLPGJ_TRANSACTION_EVENT_COMMIT		1
 #define PLPGJ_TRANSACTION_EVENT_ROLLBACK	2
 #define PLPGJ_TRANSACTION_EVENT_BEGIN		3
