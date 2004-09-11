d9ef10fe9e278ac2d26c42f7259fe3eb5db8811a
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-09-11 13:26:07 +0000

    jdbc 'prepare' added

--- pl-j/src/blocks/chanells/febe/src/java/org/plj/chanells/febe/msg/jdbc/PrepareMessageFactory.java
+++ pl-j/src/blocks/chanells/febe/src/java/org/plj/chanells/febe/msg/jdbc/PrepareMessageFactory.java
@@ -1,58 +1,56 @@
 /*
- * Created on Mar 26, 2004
+ * Created on Sep 2, 2004
  */
-
 package org.plj.chanells.febe.msg.jdbc;
 
 import java.io.IOException;
 
+import org.pgj.CommunicationException;
 import org.pgj.messages.Message;
-import org.pgj.messages.SimpleSQL;
+import org.pgj.messages.SQLPrepare;
 import org.pgj.typemapping.MappingException;
 import org.plj.chanells.febe.core.Encoding;
 import org.plj.chanells.febe.core.PGStream;
-import org.plj.chanells.febe.msg.MessageFactory;
-
 
 /**
- * Sends SQL messages to the RDBMS.
  * @author Laszlo Hornyak
  */
-class SimpleSQLMessageFactory extends AbstractSQLMessageFactory {
-
-	public static final int MESSAGE_SQLHEADER_SIMPLE = 'S';
+public class PrepareMessageFactory extends AbstractSQLMessageFactory {
 
 	/* (non-Javadoc)
-	 * @see org.plj.chanells.febe.msg.MessageFactory#getMessageHeader()
+	 * @see org.plj.chanells.febe.msg.jdbc.AbstractSQLMessageFactory#getSQLType()
 	 */
-	public int getMessageHeader() {
-		return MESSAGE_SQLHEADER_SIMPLE;
+	public int getSQLType() {
+		return SQLTYPE_PREPARE;
 	}
 
 	/* (non-Javadoc)
 	 * @see org.plj.chanells.febe.msg.MessageFactory#getMessage(org.plj.chanells.febe.core.PGStream, org.plj.chanells.febe.core.Encoding)
 	 */
 	public Message getMessage(PGStream stream, Encoding encoding)
-			throws IOException, MappingException {
-		//this hould never happen.
-		return null;
+			throws IOException, MappingException, CommunicationException {
+		throw new CommunicationException("can't get a message of that kind from the DBMS");
 	}
 
 	/* (non-Javadoc)
 	 * @see org.plj.chanells.febe.msg.MessageFactory#sendMessage(org.pgj.messages.Message, org.plj.chanells.febe.core.PGStream)
 	 */
 	public void sendMessage(Message msg, PGStream stream) throws IOException,
-			MappingException {
-		SimpleSQL sql = (SimpleSQL) msg;
-		String strsql = sql.getSql();
-		byte[] bsql = null;
-		if (strsql == null) {
-			bsql = new byte[0];
-		} else {
-			bsql = strsql.getBytes();
+			MappingException, CommunicationException {
+		SQLPrepare prepare = (SQLPrepare)msg;
+		byte[] prep = prepare.getStatement().getBytes();
+		stream.SendInteger(prep.length,4);
+		stream.Send(prep);
+
+		int sz = prepare.getParamtypes().size();
+		stream.SendInteger(sz, 4);
+
+		for(int i = 0; i< sz; i++){
+			String tp = (String) prepare.getParamtypes().get(i);
+			byte[] typ = tp.getBytes();
+			stream.SendInteger(typ.length, 4);
+			stream.Send(typ);
 		}
-		stream.SendInteger(bsql.length, 4);
-		stream.Send(bsql);
 	}
 
 	/* (non-Javadoc)
@@ -61,13 +59,6 @@ class SimpleSQLMessageFactory extends AbstractSQLMessageFactory {
 	public String getHandledClassname() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.plj.chanells.febe.msg.jdbc.AbstractSQLMessageFactory#getSQLType()
-	 */
-	public int getSQLType() {
-		return SQLTYPE_STATEMENT;
 	}
 
 }
