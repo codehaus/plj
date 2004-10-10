b6b22d17bef3eaf1c1e950df470610c530352084
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-10-10 11:02:49 +0000

    try and catch backported

--- trunk/pl-j/csrc/include/pljelog.h
+++ trunk/pl-j/csrc/include/pljelog.h
@@ -2,6 +2,7 @@
 #define PLJELOG_H
 
 #include "postgres.h"
+#include <setjmp.h>
 #include "c.h"
 #include "utils/elog.h"
 
@@ -23,6 +24,40 @@
 
 extern short pljloging;
 extern short pljlogging_error;
+
+
+extern sigjmp_buf* PG_exception_stack;
+
+/**
+ * copy-paste from pg 8.0 (some kind of backport...)
+ */
+
+#define PG_TRY()  \
+        do { \
+                sigjmp_buf *save_exception_stack = PG_exception_stack; \
+                ErrorContextCallback *save_context_stack = error_context_stack; \
+                sigjmp_buf local_sigjmp_buf; \
+                if (sigsetjmp(local_sigjmp_buf, 0) == 0) \
+                { \
+                        PG_exception_stack = &local_sigjmp_buf
+
+#define PG_CATCH()      \
+                } \
+                else \
+                { \
+                        PG_exception_stack = save_exception_stack; \
+                        error_context_stack = save_context_stack
+
+#define PG_END_TRY()  \
+                } \
+                PG_exception_stack = save_exception_stack; \
+                error_context_stack = save_context_stack; \
+        } while (0)
+
+#define PG_RE_THROW()  \
+        siglongjmp(*PG_exception_stack, 1)
+
+
 
 
 #define pljelog		if(pljloging) elog
