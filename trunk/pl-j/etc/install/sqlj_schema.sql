214fd7f2f89b7d7d50b5e24308f2342ab9c229bc
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2004-10-13 19:56:03 +0000

    management methods added

--- trunk/pl-j/etc/install/sqlj_schema.sql
+++ trunk/pl-j/etc/install/sqlj_schema.sql
@@ -3,3 +3,52 @@ drop schema sqlj;
 
 create schema sqlj;
 
+
+
+create table sqlj.plj_classes(
+	jar		varchar(255) default null,
+	fqn		varchar(255) not null primary key,
+	classcode	bytea not null,
+	created		timestamp not null default now(),
+	lmodified	timestamp,
+	cuser		varchar not null default current_user,
+	lmod		varchar
+);
+
+COMMENT ON TABLE sqlj.plj_classes is 
+'
+Used by the default JDBC classloader to store classes.
+Please never manipulate this table directly! Use standard based methods!
+
+';
+
+
+create index plj_classes_jar_idx on sqlj.plj_classes(jar);
+create index plj_classes_jar_fqn on sqlj.plj_classes(fqn);
+
+
+create or replace function sqlj.install_jar(varchar, varchar, int4) returns void as '
+	class=#privileged-class
+	method=install_jar
+' language 'plj' security definer;
+
+create function sqlj.replace_jar(varchar, varchar) returns void as
+'
+	class=#privileged-class
+	method=replace_jar
+' language 'plj' security definer;
+
+create function sqlj.remove_jar(varchar, int4) returns void as
+'
+	class=#privileged-class
+	method=remove_jar
+' language 'plj' security definer;
+
+create function sqlj.alter_java_path(varchar, varchar) returns void as
+'
+	class=#privileged-class
+	method=alter_java_path
+' language 'plj' security definer;
+
+
+
