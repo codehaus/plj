5cfbb088edeb6b3bff2f2e291e524a92fd4cb670
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2005-02-05 01:58:29 +0000

    *** empty log message ***

--- pl-j/src/tests/src/java/org/deadcat_enterprises/PLJTriggers.java
+++ pl-j/src/tests/src/java/org/deadcat_enterprises/PLJTriggers.java
@@ -15,32 +15,32 @@ public class PLJTriggers {
 
 	private static Category cat = Category.getInstance(PLJTriggers.class);
 
-	public TestTableRecord testTableInsertTrigger(TestTableRecord _new) {
+	public static TestTableRecord testTableInsertTrigger(TestTableRecord _new) {
 		cat.warn("id: "+_new.getId());
 		cat.warn("adding to testtable: " + _new.getName());
 		_new.setName("(checked by trigger)".concat(_new.getName()));
 		return _new;
 	}
 
-	public void testTableUpdateTrigger(TestTableRecord _old,
+	public static void testTableUpdateTrigger(TestTableRecord _old,
 			TestTableRecord _new) {
 		cat.warn("the entry: " + _old.getName() + " is modified to "
 				+ _new.getName());
 	}
 
-	public TestTableRecord testTable2InsertTrigger(TestTableRecord _new) {
+	public static TestTableRecord testTable2InsertTrigger(TestTableRecord _new) {
 		cat.warn("adding to testtable: " + _new.getName());
 		_new.getName().replaceAll("kocka", "K0CK@");
 		return _new;
 	}
 
-	public TestTableRecord beforeInsertRowTrigger(TestTableRecord _new) {
+	public static TestTableRecord beforeInsertRowTrigger(TestTableRecord _new) {
 		cat.warn("before insert trigger - testtable2: " + _new.getName() + " with id "+_new.getId());
 		_new.setName(_new.getName().concat("(before)"));
 		return _new;
 	}
 	
-	public TestTableRecord afterInsertRowTrigger(TestTableRecord _new) {
+	public static TestTableRecord afterInsertRowTrigger(TestTableRecord _new) {
 		cat.warn("after insert trigger - testtable2: " + _new.getName() + " with id "+_new.getId());
 		_new.setName(_new.getName().concat("(before)"));
 		return _new;
