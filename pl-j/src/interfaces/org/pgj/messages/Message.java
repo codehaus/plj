2c3bd660aa249db26588acb3468819ac4e8f209d
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2005-02-21 22:40:57 +0000

    deprecated properties removed

--- pl-j/src/interfaces/org/pgj/messages/Message.java
+++ pl-j/src/interfaces/org/pgj/messages/Message.java
@@ -1,3 +1,4 @@
+
 package org.pgj.messages;
 
 import org.pgj.Client;
@@ -9,62 +10,10 @@ import org.pgj.Client;
  */
 public abstract class Message {
 
-	private String protocol_version = null;
-	private String pgj_version = null;
-	private String jvm = null;
-
 	/**
 	 * Client MUST be set.
 	 */
 	Client client = null;
-
-	/**
-	 * Constructs a message.
-	 */
-	public Message() {
-	}
-
-	/**
-	 * Tells the protocol version of the message.
-	 */
-	public String getProtocolVersion() {
-		return protocol_version;
-	}
-
-	/**
-	 * Sets the protocol version.
-	 */
-	public void setProtocolVersion(String p) {
-		protocol_version = p;
-	}
-
-	/**
-	 * Set <b>PGJ</b> version string.
-	 */
-	public void setPGJVersion(String p) {
-		pgj_version = p;
-	}
-
-	/**
-	 * Returns <b>PGJ</b> version string.
-	 */
-	public String getPGJVersion() {
-		return pgj_version;
-	}
-
-	/**
-	 * Sets the Java Virtual Machine version string.
-	 */
-	public void setJVMVersion(String s) {
-		jvm = s;
-	}
-
-	/**
-	 * Returns the the Java Virtual Machine version string.
-	 */
-	public String getJVMVersion() {
-		return jvm;
-	}
 
 	public Client getClient() {
 		return client;
