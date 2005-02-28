2cd8cc6d0b214b7a1580d6ba78a30b2c5f1e3a6d
kocka <kocka@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2005-02-28 12:34:01 +0000

    Added jsproc tags.

--- trunk/pl-j/src/tests/src/java/org/deadcat/typetests/BooleanTests.java
+++ trunk/pl-j/src/tests/src/java/org/deadcat/typetests/BooleanTests.java
@@ -21,25 +21,44 @@ public class BooleanTests {
 		super();
 	}
 
+	/**
+	 * jsproc.udf name="plj_tests_bool_set"
+	 */
 	public static void setBoolean(Boolean b) {
 		logger.debug("setBoolean " + String.valueOf(b));
 	}
 
+	/**
+	 * Returns FALSE.
+	 * jsproc.udf name="plj_tests_bool_get"
+	 */
 	public static Boolean getBoolean() {
 		logger.debug("getBoolean");
 		return Boolean.FALSE;
 	}
 
+	/**
+	 * Performs OR operation on two Booleans.
+	 * jsproc.udf name="plj_tests_bool_or"
+	 */
 	public static Boolean doOR(Boolean a, Boolean b) {
 		return (a.booleanValue() || b.booleanValue()) ? Boolean.TRUE
 				: Boolean.FALSE;
 	}
-	
+
+	/**
+	 * Performs AND operation on two Booleans.
+	 * jsproc.udf name="plj_tests_bool_and"
+	 */
 	public static Boolean doAND(Boolean a, Boolean b) {
 		return (a.booleanValue() && b.booleanValue()) ? Boolean.TRUE
 				: Boolean.FALSE;
 	}
-	
+
+	/**
+	 * Performs negation on a Boolean.
+	 * jsproc.udf name="plj_tests_bool_not"
+	 */
 	public static Boolean doNOT(Boolean b){
 		return b.booleanValue() ? Boolean.FALSE : Boolean.TRUE;
 	}
