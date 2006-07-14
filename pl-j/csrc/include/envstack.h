cc0113ad195a6fd1c6965764f8e89d3165a535f6
nobody <nobody@6d41ac6a-ab72-0410-a1da-d78a5ef8a560>
2006-07-14 21:53:53 +0000

    This commit was manufactured by cvs2svn to create tag 'c'.

--- pl-j/csrc/include/envstack.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENVSTACK_H
-#define ENVSTACK_H
-
-void envstack_push(void);
-void envstack_pop(void);
-int envstack_isempty(void);
-void envstack_clean(void);
-
-#endif
--- pl-j/csrc/pljloging.c
+++
@@ -1,5 +0,0 @@
-
-#include "pljelog.h"
-
-short		pljloging = 1;
-short		pljlogging_error = 0;
